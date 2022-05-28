8a5deebac07fced4737be402b9fbb4a6cce9a3b1
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2022-05-28 20:29:16 +0200

    name search

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,19 +1,37 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) throws Exception {
 
-        File file = new File("C:\\TSzi\\simple-search\\test.txt");
+        File file = new File("C:\\TzSi\\pl.tomasz\\zakresdat.txt");
 
-        BufferedReader bufRead = new BufferedReader(new FileReader(file));
+        BufferedReader buffRead = new BufferedReader(new FileReader(file));
 
         String str;
 
-        while((str = bufRead.readLine()) != null){
-            System.out.println(str);
+        ArrayList<String> list = new ArrayList<>();
+
+        String searchedName = "Inna";
+
+        while ((str = buffRead.readLine()) != null) {
+
+            if (str.contains(searchedName)) {
+                String[] splittedStr = str.split(";");
+
+                for (int i = 2; i < splittedStr.length; i++) {
+                    if (splittedStr[i].contains(searchedName)) {
+                        String dateAndTime = splittedStr[0] + " " + splittedStr[i - 2] + " " + splittedStr[i];
+                        list.add(dateAndTime);
+                    }
+                }
+            }
         }
 
+        for (String meetings:list){
+            System.out.println(meetings);
+        }
     }
 }
