c2980e7e6917aad43e7bd19f98ed35d56fa254f0
Tomasz <tomasz.szymanowski@gmail.com>
2022-12-23 14:31:56 +0100

    #3 added GUI sketch

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,7 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class App {
-    private JPanel panel1;
-    private JButton searchButton;
-    private JTextField textField1;
-    private JTextPane textPane1;
-    private JLabel infoLabel;
-
-
-    public App() {
-        searchButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String name = textField1.getText();
-                textPane1.setText(name);
-            }
-        });
-    }
-
-    public static void main(String[] args) {
-        App app = new App();
-
-    }
+    private JTextField nazwiskoKursantaTextField;
+    private JTextArea textArea1;
+    private JButton wyszukujeTerminyButton;
 }
