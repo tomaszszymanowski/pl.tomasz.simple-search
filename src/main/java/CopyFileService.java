404b35ed4fbcd95eadb804ef53cfd27472c0a773
tomaszszymanowski <Tomasz.Szymanowski@gmail.com>
2023-04-30 09:57:47 +0200

    #3 dodanie nowej wersji bilbioteki naprawiło problem

--- src/main/java/CopyFileService.java
+++ src/main/java/CopyFileService.java
@@ -2,13 +2,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class CopyFileService {
 
     static void copySourceFile() throws IOException {
         File src = new File("C:\\Users\\Tomasz\\OneDrive\\Ab-As\\auto start\\1 Jazdy Spis.xlsx");
-        File dest = new File("C:\\TzSi\\AKTUALNE_PROJEKTY\\pl.tomasz.simple-search\\1 Jazdy Spis.xlsx");
+        File dest = new File("C:\\TSzi\\simple-search\\1 Jazdy Spis.xlsx");
 
         // using copy(InputStream,Path Target); method
-        Files.copy(src.toPath(), dest.toPath());
+        Files.copy(src.toPath(), dest.toPath(),REPLACE_EXISTING);
     }
 }
